4f5b99c9818584408c0f4c717d6ddb0288de7352
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-29 20:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function breakOut(array, changeValue, stopValue){
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === 1) {
-      array[i] = 'c'
+    if (array[i] === stopValue) {
+      break
     }
+    array[i] = 'c'
   }
 }
